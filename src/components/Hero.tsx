3eb1a04cec890172e1fcf4c4ab4d102ada5bef95
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-13 13:18:00 +0000

    レイアウト調整

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className="inline-block space-y-1">
-            <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">さぁ、</h1>
-            <br />
-            <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">レイトチェックアウトホテルを</h1>
-            <br />
-            <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">検索しよう</h1>
-        </div>
+            <div className="space-y-1 ">
+                <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">さぁ、</h1>
+                <br />
+                <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">レイトチェックアウトホテルを</h1>
+                <br />
+                <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">検索しよう</h1>
+            </div>
     )
 }
 
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -5,10 +5,14 @@ import SearchBox from '../components/SearchBox'
 
 const TopPage = () => {
     return (
-        <div>
+        <div className="relative object-center max-h-screen min-h-screen bg-center bg-cover bg-hero-image">
             <NavBar></NavBar>
-            <Hero/>
-            <SearchBox/>
+            <div className="flex items-center justify-center h-screen transform -translate-y-32">
+                <Hero/> 
+            </div>
+            <div className="absolute inset-x-0 bottom-0 mx-4 mb-4 ">
+                <SearchBox/>
+            </div>
         </div>
     )
 }
