482f094e14a6b86b1622f166c6b268a6621bbdfb
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-13 15:25:33 +0000

    一旦ホテル一覧ページを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopPage from './pages/TopPage';
+import HotelListPage from './pages/HotelListPage';
 
 function App() {
   return (
     <div className="App">
         <TopPage/>
+        <HotelListPage></HotelListPage>
     </div>
   );
 }
