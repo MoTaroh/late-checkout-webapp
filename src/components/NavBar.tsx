d0dc55fd270d63ccb789b6fcf9082b976f752d28
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-13 13:18:34 +0000

    固定

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div className="flex items-center justify-between h-16 px-4">
+        <div className="container fixed flex items-center justify-between h-16 px-4 py-4 text-white">
             <h1 className="text-2xl font-bold">LOGO</h1>
             <div>info</div>
         </div>
