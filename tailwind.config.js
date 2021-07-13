c22eda237a2889a1970079996d7c6be890b6b8ca
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-13 12:51:14 +0000

    背景画像を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "hotel-green": "#5F98A3",
+        },
+        backgroundImage: theme => ({
+            "hero-image": "url('./images/hero_bg.jpg')",
+        }),
+    },
+    container: {
+        center: true,
+        padding: '1rem',
+    }
   },
   variants: {
     extend: {},
