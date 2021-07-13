d5b79d2c721bb22e2ed1e0088e380de1763f26a4
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-13 12:51:05 +0000

    左揃えに修正

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen ">
-            <h1 className="px-2 py-1 font-bold text-white bg-gray-800 sm:text-xl md:text-3xl bg-opacity-20">さぁ、</h1>
-            <h1 className="px-2 py-1 font-bold text-white bg-gray-800 sm:text-xl md:text-3xl bg-opacity-20">レイトチェックアウトホテルを</h1>
-            <h1 className="px-2 py-1 font-bold text-white bg-gray-800 sm:text-xl md:text-3xl bg-opacity-20">検索しよう</h1>
+        <div className="inline-block space-y-1">
+            <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">さぁ、</h1>
+            <br />
+            <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">レイトチェックアウトホテルを</h1>
+            <br />
+            <h1 className="inline-block px-2 py-1 text-xl font-bold text-white bg-gray-800 md:text-3xl bg-opacity-20">検索しよう</h1>
         </div>
     )
 }
