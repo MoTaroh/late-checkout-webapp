8a9a72fc38a406b2106df640ca591c50be9eb0cc
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-14 13:13:17 +0000

    Footer追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TopPage from './pages/TopPage';
 import HotelListPage from './pages/HotelListPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
         <TopPage/>
         <HotelListPage></HotelListPage>
+        <Footer></Footer>
     </div>
   );
 }
