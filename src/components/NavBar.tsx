6eda1af31be76649da827a3b0f63f6df774e9818
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-14 15:36:49 +0000

    アイコン導入

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {InformationCircleIcon} from '@heroicons/react/outline'
 
 const NavBar = () => {
     return (
         <div className="flex items-center justify-between h-16 px-4 py-4 text-white">
             <h1 className="text-2xl font-bold">LOGO</h1>
-            <div>info</div>
+            <InformationCircleIcon className="w-6 h-6 text-white"></InformationCircleIcon>
         </div>
     )
 }
