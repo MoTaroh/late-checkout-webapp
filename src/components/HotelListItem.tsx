8612e1beb18cdbd57927cdc682d0540a6d4dbddb
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-14 13:13:34 +0000

    mapでホテルリストを表示

--- src/components/HotelListItem.tsx
+++ src/components/HotelListItem.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
-const HotelListItem = () => {
+
+// TODO: 親でホテルリストをmapして読み込むので、ホテル情報を受け取る
+// TODO: 検索情報（日付と都道府県と人数）を受け取る
+// TODO: 受け取った情報をもとにURLを生成する
+// TODO: 要素全体にクリックイベントを設定し、URLを別タブで開く
+
+
+
+const HotelListItem = (props: any) => {
+    const url = `https://www.jalan.net/yad${props.hotelInfo.hotelNo}/plan/?screenId=UWW3101&yadNo=${props.hotelInfo.hotelNo}&reSearchFlg=1&roomCrack=200000&smlCd=272005&distCd=01&stayYear=2021&stayMonth=8&stayDay=12&stayCount=1&roomCount=1&adultNum=${props.searchInfo.personNum}&minPrice=0&maxPrice=999999`
+    // クリックイベント
     return (
-        <div className="flex items-center justify-between px-6 py-4 bg-white border border-gray-300 rounded-xl">
+        <a href={url} rel="noopener noreferrer" target="_blank" className="flex items-center justify-between px-6 py-4 bg-white border border-gray-300 cursor-pointer rounded-xl">
             <div className="flex flex-col">
-                <h3 className="text-sm font-bold">ホテルマイステイズ御堂筋本町</h3>
-                <span className="mt-3 text-sm font-bold text-hotel-green">10月14日のプラン一覧</span>
+                <h3 className="text-sm font-bold">{props.hotelInfo.hotelName}</h3>
+                <span className="mt-3 text-sm font-bold text-hotel-green">{props.searchInfo.date}のプラン一覧</span>
             </div>
             <div className="text-hotel-green">
                 ＞
             </div>
-        </div>
+        </a>
     )
 }
 
--- src/pages/HotelListPage.tsx
+++ src/pages/HotelListPage.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import HotelListItem from '../components/HotelListItem'
 
+const hotelList = [
+    {
+        "hotelName": "ホテルマイステイズ御堂筋本町",
+        "hotelNo": "314648"
+    },
+    {
+        "hotelName": "ホテルマイステイズ堺筋本町",
+        "hotelNo": "314648"
+    },
+    {
+        "hotelName": "アパホテル　淀屋橋　北浜駅前",
+        "hotelNo": "314648"
+    },
+    {
+        "hotelName": "ホテルグレイスリー大阪なんば",
+        "hotelNo": "314648"
+    },
+    {
+        "hotelName": "ホテルヒラリーズ心斎橋",
+        "hotelNo": "314648"
+    },
+]
+
+const searchInfo = {
+    "date": "10月16日",   // TODO: datetimeを使用
+    "prefecture": "大阪府",
+    "personNum": 2
+}
+
 const HotelListPage = () => {
     return (
         <div className="container flex flex-col bg-gray-100">
             <h2 className="mt-5 mb-6 text-lg font-bold text-gray-800">ホテル一覧</h2>
-            <HotelListItem></HotelListItem>
+            <div className="flex flex-col mb-6 space-y-3">
+                {
+                    hotelList.map(hotelInfo => (
+                        <HotelListItem key={hotelInfo.hotelName} hotelInfo={hotelInfo} searchInfo={searchInfo}></HotelListItem>
+                    ))
+                }
+            </div>
         </div>
     )
 }
