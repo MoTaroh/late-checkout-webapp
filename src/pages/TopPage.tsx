7733f3ef225f4c1d21d531b2a76a113c59a8bae8
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-16 12:42:26 +0000

    調整

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -7,7 +7,7 @@ const TopPage = () => {
     return (
         <div className="container relative object-center max-h-screen min-h-screen bg-center bg-cover bg-hero-image">
             <NavBar></NavBar>
-            <div className="flex items-center justify-center h-screen transform -translate-y-32">
+            <div className="z-0 flex items-center justify-center transform translate-y-48">
                 <Hero/> 
             </div>
             <div className="absolute inset-x-0 bottom-0 mx-4 mb-4 ">
