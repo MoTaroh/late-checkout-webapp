996c0f6f8c9d7a5b0a7c3225423c7d4f9b07da6e
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-16 11:54:19 +0000

    ホテル情報を追加

--- src/pages/HotelListPage.tsx
+++ src/pages/HotelListPage.tsx
@@ -5,16 +5,24 @@ import Footer from '../components/Footer'
 import HotelListItem from '../components/HotelListItem'
 
 const hotelList = [
+    {"hotelName": "アインズ・イン梅田東","hotelNo": "345754"},
+    {"hotelName": "アパホテル〈御堂筋本町駅前〉","hotelNo": "386787"},
+    {"hotelName": "アパホテル〈御堂筋本町駅東〉（全室禁煙）","hotelNo": "386585"},
+    {"hotelName": "アパホテル〈大阪肥後橋駅前〉（全室禁煙）","hotelNo": "346744"},
+    {"hotelName": "アパホテル〈東梅田 南森町駅前〉","hotelNo": "323812"},
+    {"hotelName": "アパホテル〈淀屋橋 北浜駅前〉","hotelNo": "369184"},
+    {"hotelName": "アパホテル＆リゾート〈御堂筋本町駅タワー〉（全室禁煙）","hotelNo": "376554"},
+    {"hotelName": "イビス大阪梅田","hotelNo": "339535"},
+    {"hotelName": "スーパーホテルPremier大阪・本町　天然温泉　四季彩の湯","hotelNo": "308796"},
+    {"hotelName": "スーパーホテル梅田・肥後橋","hotelNo": "328798"},
+    {"hotelName": "プレミアホテル－CABIN－大阪","hotelNo": "328778"},
+    {"hotelName": "ホテルＷＢＦ本町","hotelNo": "377707"},
+    {"hotelName": "ホテルマイステイズプレミア堂島","hotelNo": "334326"},
     {"hotelName": "ホテルマイステイズ御堂筋本町","hotelNo": "314648"},
-    {"hotelName": "ホテルマイステイズ堺筋本町","hotelNo": "314648"},
-    {"hotelName": "アパホテル　淀屋橋　北浜駅前","hotelNo": "314648"},
-    {"hotelName": "ホテルグレイスリー大阪なんば","hotelNo": "314648"},
-    {"hotelName": "ホテルヒラリーズ心斎橋","hotelNo": "314648"},
-    {"hotelName": "ホテルマイステイズ御堂筋本町2","hotelNo": "314648"},
-    {"hotelName": "ホテルマイステイズ堺筋本町2","hotelNo": "314648"},
-    {"hotelName": "アパホテル　淀屋橋　北浜駅前2","hotelNo": "314648"},
-    {"hotelName": "ホテルグレイスリー大阪なんば2","hotelNo": "314648"},
-    {"hotelName": "ホテルヒラリーズ心斎橋2","hotelNo": "314648"},
+    {"hotelName": "ホテルマイステイズ堺筋本町","hotelNo": "304638"},
+    {"hotelName": "東急ステイ大阪本町（2020年2月27日グランドオープン）","hotelNo": "307894"},
+    {"hotelName": "ホテルグレイスリー大阪なんば","hotelNo": "330578"},
+    {"hotelName": "ホテルヒラリーズ心斎橋","hotelNo": "363847"},
 ]
 
 const HotelListPage = () => {
