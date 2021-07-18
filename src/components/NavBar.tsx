a4b2bcdcf4a4788a90af832988d17d8ba1c8f3ac
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-18 02:40:12 +0000

    infoボタンクリック時にポップアップを表示

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,11 @@
-import React, {useState} from 'react'
-import {InformationCircleIcon} from '@heroicons/react/outline'
+import React from 'react'
+import InfoPopover from './InfoPopover'
 
 const NavBar = () => {
-    const [infoIsOpen, setInfoIsOpen] = useState(false)
-    const handleClick = () => {
-        setInfoIsOpen(prevState => !prevState)
-        console.log(infoIsOpen)
-    }
     return (
         <div className="flex items-center justify-between h-16 py-4 text-white">
             <h1 className="text-2xl font-bold">LOGO</h1>
-            <button onClick={handleClick}>
-                <InformationCircleIcon className="w-6 h-6 text-white"></InformationCircleIcon>
-            </button>
+            <InfoPopover color="text-white"></InfoPopover>
         </div>
     )
 }
--- src/components/NavBar2.tsx
+++ src/components/NavBar2.tsx
@@ -1,20 +1,13 @@
-import React, {useState} from 'react'
-import {InformationCircleIcon} from '@heroicons/react/outline'
+import React from 'react'
+import InfoPopover from './InfoPopover'
 
 // TODO: ひどすぎる実装なので後ほど修正する
 
 const NavBar2 = () => {
-    const [infoIsOpen, setInfoIsOpen] = useState(false)
-    const handleClick = () => {
-        setInfoIsOpen(prevState => !prevState)
-        console.log(infoIsOpen)
-    }
     return (
         <div className="flex items-center justify-between h-16 py-4 text-gray-600">
             <h1 className="text-2xl font-bold ">LOGO</h1>
-            <button onClick={handleClick}>
-                <InformationCircleIcon className="w-6 h-6"></InformationCircleIcon>
-            </button>
+            <InfoPopover color="text-gray-600"></InfoPopover>
         </div>
     )
 }
