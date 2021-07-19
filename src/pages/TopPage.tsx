3703004634e96016d0ab1d1902053c95cfab3b35
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-19 13:05:08 +0000

    bottom-1/8追加

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -11,7 +11,7 @@ const TopPage = () => {
                 <div className="flex items-center justify-center transform translate-y-48 lg:justify-start">
                     <Hero/> 
                 </div>
-                <div className="absolute inset-x-0 bottom-0 mx-4 mb-4 lg:mb-40">
+                <div className="absolute inset-x-0 bottom-0 mx-4 mb-4 lg:bottom-1/8">
                     <SearchBox/>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,26 @@ module.exports = {
         gray: colors.blueGray,
         white: colors.white
     },
+    inset: (theme, { negative }) => ({
+        auto: 'auto',
+        ...theme('spacing'),
+        ...negative(theme('spacing')),
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/8': '12.5%',
+        '1/4': '25%',
+        '2/4': '50%',
+        '3/4': '75%',
+        full: '100%',
+        '-1/2': '-50%',
+        '-1/3': '-33.333333%',
+        '-2/3': '-66.666667%',
+        '-1/4': '-25%',
+        '-2/4': '-50%',
+        '-3/4': '-75%',
+        '-full': '-100%',
+      }),
     extend: {
         colors: {
             "hotel-green": "#5F98A3",
