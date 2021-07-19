083192e0489650b22ceecc30670dc8916608cd31
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-07-19 14:38:22 +0000

    レスポンシブ調整

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -6,12 +6,12 @@ import SearchBox from '../components/SearchBox'
 const TopPage = () => {
     return (
         <div className="object-center bg-center bg-cover bg-hero-image">
-            <div className="container relative max-h-screen min-h-screen">
+            <div className="container relative max-h-screen min-h-screen lg:px-12">
                 <NavBar></NavBar>
                 <div className="flex items-center justify-center transform translate-y-48 lg:justify-start">
                     <Hero/> 
                 </div>
-                <div className="absolute inset-x-0 bottom-0 mx-4 mb-4 lg:bottom-1/8">
+                <div className="absolute inset-x-0 bottom-0 flex justify-center px-4 mb-4 lg:px-12 lg:bottom-1/8">
                     <SearchBox/>
                 </div>
             </div>
