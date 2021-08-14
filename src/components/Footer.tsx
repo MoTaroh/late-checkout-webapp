6976e755f977503cfcd523aae9d2a250ffd6547d
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-08-14 14:42:52 +0000

    ディバイダー追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="py-1 text-xs text-center text-gray-400 bg-gray-100">
-            © 2021 MoTaroh
-        </div>
+        <>
+            <div className="w-full h-px bg-gray-300"></div>
+            <div className="py-4 text-xs text-center text-gray-400 bg-gray-100">
+                © 2021 MoTaroh
+            </div>
+        </>
     )
 }
 
