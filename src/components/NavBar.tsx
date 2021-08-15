44603b30039b9e828f23427bc0eb18e9ea884a59
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-08-15 10:18:02 +0000

    ロゴタイトル変更

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import InfoPopover from './InfoPopover'
 const NavBar = () => {
     return (
         <div className="flex items-center justify-between h-16 py-4 text-white">
-            <h1 className="text-2xl font-bold">LOGO</h1>
+            <h1 className="text-2xl font-bold">LongSty</h1>
             <InfoPopover color="text-white"></InfoPopover>
         </div>
     )
