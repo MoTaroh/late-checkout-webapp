5912958cbb0bfdc68625df64bdda249045e09e9f
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-08-15 10:17:53 +0000

    固定に変更

--- src/components/NavBar2.tsx
+++ src/components/NavBar2.tsx
@@ -5,9 +5,11 @@ import InfoPopover from './InfoPopover'
 
 const NavBar2 = () => {
     return (
-        <div className="flex items-center justify-between h-16 py-4 text-gray-600">
-            <h1 className="text-2xl font-bold ">LOGO</h1>
-            <InfoPopover color="text-gray-600"></InfoPopover>
+        <div className="fixed inset-x-0 top-0 z-50 h-16 py-4 bg-white shadow text-hotel-green md:h-20">
+            <div className="container flex items-center justify-between">
+                <h1 className="text-2xl font-bold ">LongSty</h1>
+                <InfoPopover color="text-gray-600"></InfoPopover>
+            </div>
         </div>
     )
 }
