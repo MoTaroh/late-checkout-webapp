7390c957a24c8f3ce5095fd5b040c5152157ed68
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2021-08-16 08:51:32 +0000

    useContext導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import TopPage from './pages/TopPage';
 import HotelListPage from './pages/HotelListPage';
 
+// 検索ボックスのContext
+export const SearchContext = createContext({
+    stayDate: new Date(),
+    handleStayDate: (date: Date) => {},
+    stayCount: 1,
+    handleStayCount: (event: any) => {},
+    personNum: 2,
+    handlePersonNum: (event: any) => {},
+});
+
+
 function App() {
+    // 検索ボックスの入力値を保持
+    // datepicker
+    const initialDate = new Date()
+    const [stayDate, setStayDate] = useState(initialDate)
+    const handleStayDate = (date: Date) => {
+        setStayDate(date)
+    }
+    // 宿泊日数選択
+    const [stayCount, setStayCount] = useState(1)
+    const handleStayCount = (event: any) => {
+        setStayCount(event.target.value);
+    }
+    // 宿泊人数（大人のみ）
+    const [personNum, setPersonNum] = useState(2)
+    const handlePersonNum = (event: any) => {
+        setPersonNum(event.target.value)
+    }
+
   return (
-      <Router>
-        <div className="App">
-            <Switch>
-                <Route path="/" exact>
-                    <TopPage/>
-                </Route>
-                <Route path="/hotels">
-                    <HotelListPage></HotelListPage>
-                </Route>
-            </Switch>
-        </div>
-      </Router>
+      <SearchContext.Provider value={{stayDate, handleStayDate, stayCount, handleStayCount, personNum, handlePersonNum}}>
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/" exact>
+                        <TopPage/>
+                    </Route>
+                    <Route path="/hotels">
+                        <HotelListPage></HotelListPage>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+      </SearchContext.Provider>
   );
 }
 
