8d939945479fa2ece13f68cec57bd14627c55e37
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-01-10 14:46:17 +0000

    ver1の名残を削除

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import InfoPopover from '../InfoPopover'
-
 const NavBar = () => {
-    return (
-        <div className="flex items-center justify-between h-16 py-4 text-white">
-            <h1 className="text-2xl font-bold">LongSty</h1>
-            <InfoPopover color="text-white"></InfoPopover>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-start h-16 py-4 text-white">
+      <h1 className="text-2xl font-bold">LongSty</h1>
+    </div>
+  );
+};
 
 export default NavBar;
