a881a4613932e689c5d7477943f3859e9aa3499d
MoTaroh <52590586+MoTaroh@users.noreply.github.com>
2022-03-19 06:14:00 +0000

    2021 -> 2022

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
       <div className="w-full h-px bg-slate-300"></div>
       <div className="py-2 text-xs text-center text-slate-400 bg-slate-100 sm:py-4">
-        © 2021 MoTaroh
+        © 2022 MoTaroh
       </div>
     </>
   );
